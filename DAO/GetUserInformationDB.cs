f6c338b773b4aa216fca01ff0548c6bed4e1a0ba
sonijay19 <sonijay1910@gmail.com>
2021-02-17 18:56:38 +0530

    Third Commit

--- DAO/GetUserInformationDB.cs
+++ DAO/GetUserInformationDB.cs
@@ -43,36 +43,42 @@ namespace RESTServices.DAO
         public List<UserInformation> GetAllUserDetails(BusinessReuqestMessage UserDetails)
         {
             List<UserInformation> UserInfo = new List<UserInformation>();
-            //string query = GetQueryByProperties.GetQuery("SortActiveUserDetails", UserStatus.All);
-            //Debug.WriteLine("Query is ",query);
-            string query = "SELECT UserDetails.Email,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId WHERE UserTypeStatus.StatusCode = @UserType AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) ORDER BY @sortbyParameter @sortbyDirection";
-            using (SqlCommand cmd = new SqlCommand(query, conn))
+            GetQueryByProperties getQuery = new GetQueryByProperties();
+            string newquery = getQuery.GetDetailQuery("SortActiveUserDetails", UserStatus.All,UserDetails.sortbyParameter,UserDetails.sortbyDirection);
+            Debug.WriteLine("Query is " + newquery);
+            string query = "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId WHERE UserTypeStatus.StatusCode = @UserType AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) ORDER BY firstname ASC;";
+            if (newquery == query)
+            {
+                Debug.WriteLine("Same to same chhe");
+            }
+            using (SqlCommand cmd = new SqlCommand(newquery, conn))
             {
                 cmd.CommandType = CommandType.Text;
                 //UserDetails.Email,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate
                 // @UserType ORDER BY @sortbyParameter @sortbyDirection @fromDate @toDate
                 conn.Open();
                 cmd.Parameters.AddWithValue("@UserType", UserDetails.AccessType);
-                cmd.Parameters.AddWithValue("@sortbyParameter", UserDetails.sortbyParameter);
-                cmd.Parameters.AddWithValue("@sortbyDirection", UserDetails.sortbyParameter);
                 cmd.Parameters.AddWithValue("@fromDate", UserDetails.FromDate);
                 cmd.Parameters.AddWithValue("@toDate", UserDetails.ToDate);
+                cmd.Parameters.AddWithValue("@sortbyParameter", UserDetails.sortbyParameter);
+                cmd.Parameters.AddWithValue("@sortbyDirection", UserDetails.sortbyDirection);
 
                 using (SqlDataReader reader = cmd.ExecuteReader())
-                {
+                { 
+                    Debug.WriteLine("Ahiya pochi jay chhe");
                     while (reader.Read())
                     {
                         UserInfo.Add(
                             new UserInformation
                             {
-                                UserEmail = reader["UserDetails.Email"].ToString(),
-                                FirstName = reader["UserDetails.firstName"].ToString(),
-                                LastName = reader["UserDetails.lastName"].ToString(),
-                                UserType = ((UserIdDeleted)Convert.ToInt16(reader["UserDetails.IsDeleted"])).ToString(),
-                                UserStatus = reader["UserTypeStatus.StatusCode"].ToString(),
+                                UserEmail = reader["UserEmail"].ToString(),
+                                FirstName = reader["firstName"].ToString(),
+                                LastName = reader["lastName"].ToString(),
+                                UserType = ((UserIdDeleted)Convert.ToInt16(reader["IsDeleted"])).ToString(),
+                                UserStatus = reader["StatusCode"].ToString(),
                             }
                         );
-
+                        Debug.WriteLine(UserInfo.Count);
                         conn.Close();
                         return UserInfo;
                     }
--- DAO/propeties/GetQueryByProperties.cs
+++ DAO/propeties/GetQueryByProperties.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using RESTful_Services.BusinessLayer.Entities.Enums;
@@ -10,21 +11,240 @@ namespace RESTful_Services.DAO.propeties
     public class GetQueryByProperties
     //FirstName LastName UserEmail UserTypeId CreatedDate StatusCode
     {
-        private static IniParser parser = new IniParser(@"G:\Training\2_16_2021\RESTful_Services\DAO\propeties\sql.ini");
-        public static string GetQuery(string proprtyName, UserStatus userStatus)
+        private IniParser parser = new IniParser(@"G:\Training\2_16_2021\RESTful_Services\DAO\propeties\sql.ini");
+        public string GetDetailQuery(string proprtyName, UserStatus userStatus,string sortbyParameter,string sortbyDirection)
         {
-            string query = String.Empty;
+            Debug.WriteLine("andar aave chhe");
+            string query;
             switch (userStatus)
             {
-                case UserStatus.Active:
-                    query = parser.GetSetting("SortActiveUserDetails", "ActiveUserDetailsSort");
-                    return query; 
-                case UserStatus.Deleted:
-                    query = parser.GetSetting("SortDeletedUserDetails", "DeletedUserDetailsSort");
-                    return query;
                 case UserStatus.All:
-                    query = parser.GetSetting("SortAllUserDetails", "AllUserDetailsSort");
-                    return query;
+                    Debug.WriteLine("user status ma aay gayu");
+                    switch (sortbyParameter)
+                    {
+                        case "firstname":
+                            switch (sortbyDirection)
+                            {
+                                case "asc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY firstname asc";
+                                    return query;
+                                case "desc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY firstname desc";
+                                    return query;
+                                default:
+                                    return null;
+                            }
+                        case "lastname":
+                            switch (sortbyDirection)
+                            {
+                                case "asc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY lastname asc";
+                                    return query;
+                                case "desc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY lastname desc";
+                                    return query;
+                                default:
+                                    return null;
+                            }
+                        case "createdDate":
+                            switch (sortbyDirection)
+                            {
+                                case "asc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY createdDate asc";
+                                    return query;
+                                case "desc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY createdDate desc";
+                                    return query;
+                                    break;
+                                default:
+                                    return null;
+                            }
+                            break;
+                    }
+                    return "not";
+                case UserStatus.Deleted:
+                    switch (sortbyParameter)
+                    {
+                        case "firstname":
+                            switch (sortbyDirection)
+                            {
+                                case "asc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 1 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY firstname asc";
+                                    return query;
+                                case "desc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 1 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY firstname desc";
+                                    return query;
+                                default:
+                                    return null;
+                            }
+                            break;
+                        case "lastname":
+                            switch (sortbyDirection)
+                            {
+                                case "asc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 1 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY lastname asc";
+                                    return query;
+                                case "desc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 1 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY lastname desc";
+                                    return query;
+                                default:
+                                    return null;
+                            }
+                            break;
+                        case "createdDate":
+                            switch (sortbyDirection)
+                            {
+                                case "asc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 1 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY createdDate asc";
+                                    return query;
+                                case "desc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 1 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY createdDate desc";
+                                    return query;
+                                    break;
+                                default:
+                                    return null;
+                            }
+                            break;
+                    }
+                    return "not";
+                case UserStatus.Active:
+                    Debug.WriteLine("Active ma aayo");
+                    switch (sortbyParameter)
+                    {
+                        case "firstname":
+                            Debug.WriteLine("case ma aay gayo");
+                            switch (sortbyDirection)
+                            {
+                                case "asc":
+                                    Debug.WriteLine("return query");
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 0 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY firstname asc";
+                                    return query;
+                                case "desc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 0 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY firstname desc";
+                                    return query;
+                                default:
+                                    return null;
+                            }
+                            break;
+                        case "lastname":
+                            switch (sortbyDirection)
+                            {
+                                case "asc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 0 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY lastname asc";
+                                    return query;
+                                case "desc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 0 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY lastname desc";
+                                    return query;
+                                default:
+                                    return null;
+                            }
+                            break;
+                        case "createdDate":
+                            switch (sortbyDirection)
+                            {
+                                case "asc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 0 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY createdDate asc";
+                                    return query;
+                                case "desc":
+                                    query =
+                                        "SELECT UserDetails.UserEmail,UserDetails.firstName,UserDetails.lastName,UserTypeStatus.StatusCode,UserDetails.CreatedDate,UserDetails.ModifiedDate,UserDetails.IsDeleted FROM UserDetails " +
+                                        "INNER JOIN UserTypeStatus ON UserTypeStatus.TypeId = UserDetails.UserTypeId " +
+                                        "WHERE UserDetails.IsDeleted = 0 AND UserTypeStatus.StatusCode = @UserType " +
+                                        "AND CreatedDate BETWEEN convert(date,@fromDate,103) and convert(date,@toDate,103) " +
+                                        "ORDER BY createdDate desc";
+                                    return query;
+                                    break;
+                                default:
+                                    return null;
+                            }
+                            break;
+                    }
+                    return "not";
                 default:
                     return null;
             }
